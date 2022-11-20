0e707d50cbdb21ecd2a41891cd332fc45e15bcc0
Zheng Hao Tan <hi@zhenghaotan.com>
2022-11-20 14:20:22 -0800

    update Cargo.toml

--- hello_world/src/lib.rs
+++ hello_world/src/lib.rs
@@ -1,11 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+    // #[test]
+    // fn it_works() {
+        // let result = 2 + 2;
+        // assert_eq!(result, 4);
+    // }
+// }
 
 use solana_program::{
     account_info::AccountInfo,
